c97b0f5c94f6d3b6b0c543b4b1de3ca27a59f262
Peter Jakubčo <pjakubco@gmail.com>
2009-01-18 09:33:20 +0100

    * Moved project to ECLIPSE environment * Removed unused comments (UML markers) in some interfaces * Change type of method getType() in IDebugColumn

--- src/plugins/cpu/IDebugColumn.java
+++ src/plugins/cpu/IDebugColumn.java
@@ -1,37 +1,25 @@
 package plugins.cpu;
 
 
-// <editor-fold defaultstate="collapsed" desc=" UML Marker "> 
-// #[regen=yes,id=DCE.E68A4167-F79E-50CE-0352-A247DD47ADD5]
-// </editor-fold> 
 /**
  * Interface that holds information about column in debug window.
  */
 public interface IDebugColumn {
 
-    // <editor-fold defaultstate="collapsed" desc=" UML Marker "> 
-    // #[regen=yes,id=DCE.D0532C73-56CD-F50D-53A0-1BD3E43F7D59]
-    // </editor-fold>
     /**
      * Gets java type of the column. Mostly the column type is <code>java.lang.String</code>,
      * but for breakpoint columns should be used <code>java.lang.Boolean</code>
      * class.
      * @return Java type of this column
      */
-    public Class getType ();
+    public Class<?> getType ();
 
-    // <editor-fold defaultstate="collapsed" desc=" UML Marker "> 
-    // #[regen=yes,id=DCE.9DE2F163-518E-731E-BBA4-CEDBBDF14227]
-    // </editor-fold> 
     /**
      * Gets name (title) of the column.
      * @return title of this column
      */
     public String getName ();
 
-    // <editor-fold defaultstate="collapsed" desc=" UML Marker "> 
-    // #[regen=yes,id=DCE.4212068E-5FE8-C678-9348-36F643FA3062]
-    // </editor-fold> 
     /**
      * Determines whether this column is editable by user. For example, mnemonics
      * column shouldn't be editable (if CPU doesn't support assembly in runtime),
