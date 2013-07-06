a48adfa2f65e053f36d1e9b235c0f71816b80748
Peter Jakubčo <peter.jakubco@zoomint.com>
2013-07-06 08:06:01 +0200

    Try to remove redundant dependencies from POM

--- scripts/addServer.py
+++ scripts/addServer.py
@@ -1,6 +1,6 @@
 #!/usr/bin/env python
 
-# Taken from gist https://gist.github.com/neothemachine/4060735
+# Modified gist https://gist.github.com/neothemachine/4060735
 
 import sys
 import os
@@ -23,18 +23,20 @@ if not serversNodes:
 else:
   serversNode = serversNodes[0]
 
-sonatypeServerNode = m2.createElement("server")
-sonatypeServerId = m2.createElement("id")
-sonatypeServerUser = m2.createElement("username")
-sonatypeServerPass = m2.createElement("password")
+repositoryServerNode = m2.createElement("server")
+repositoryServerId = m2.createElement("id")
+repositoryServerUser = m2.createElement("username")
+repositoryServerPass = m2.createElement("password")
  
 idNode = m2.createTextNode("emustudio-repository")
 userNode = m2.createTextNode(os.environ["EMUSTUDIO_USERNAME"])
 passNode = m2.createTextNode(os.environ["EMUSTUDIO_PASSWORD"])
  
-sonatypeServerId.appendChild(idNode)
-sonatypeServerUser.appendChild(userNode)
-sonatypeServerPass.appendChild(passNode)
+repositoryServerId.appendChild(idNode)
+repositoryServerUser.appendChild(userNode)
+repositoryServerPass.appendChild(passNode)
+
+# Try to disable strict host checking
 
 configNode = m2.createElement("configuration")
 knownHostsNode = m2.createElement("knownHostsProvider")
@@ -45,12 +47,12 @@ disableHostValue = m2.createTextNode("no")
 disableHostNode.appendChild(disableHostValue)
 knownHostsNode.appendChild(disableHostNode)
  
-sonatypeServerNode.appendChild(sonatypeServerId)
-sonatypeServerNode.appendChild(sonatypeServerUser)
-sonatypeServerNode.appendChild(sonatypeServerPass)
-sonatypeServerNode.appendChild(configNode)
+repositoryServerNode.appendChild(repositoryServerId)
+repositoryServerNode.appendChild(repositoryServerUser)
+repositoryServerNode.appendChild(repositoryServerPass)
+repositoryServerNode.appendChild(configNode)
  
-serversNode.appendChild(sonatypeServerNode)
+serversNode.appendChild(repositoryServerNode)
 
 # Turn off interactive mode
 interactiveNode = m2.createElement("interactiveMode")
@@ -63,6 +65,6 @@ f = open(homedir + '/.m2/mySettings.xml', 'w')
 f.write(m2Str)
 f.close()
 
-# append sourceforge.net public ssh key fingerprint
+# append sourceforge.net public ssh key fingerprint (since disabling strict host checking doesn't work)
 with open(homedir + "/.ssh/known_hosts", "a") as knownHostsFile:
     knownHostsFile.write("web.sourceforge.net,216.34.181.70 ssh-rsa AAAAB3NzaC1yc2EAAAABIwAAAQEA2uifHZbNexw6cXbyg1JnzDitL5VhYs0E65Hk/tLAPmcmm5GuiGeUoI/B0eUSNFsbqzwgwrttjnzKMKiGLN5CWVmlN1IXGGAfLYsQwK6wAu7kYFzkqP4jcwc5Jr9UPRpJdYIK733tSEmzab4qc5Oq8izKQKIaxXNe7FgmL15HjSpatFt9w/ot/CHS78FUAr3j3RwekHCm/jhPeqhlMAgC+jUgNJbFt3DlhDaRMa0NYamVzmX8D47rtmBbEDU3ld6AezWBPUR5Lh7ODOwlfVI58NAf/aYNlmvl2TZiauBCTa7OPYSyXJnIPbQXg6YQlDknNCr0K769EjeIlAfY87Z4tw==")
