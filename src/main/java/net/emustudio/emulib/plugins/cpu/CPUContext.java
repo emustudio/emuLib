b5fe1bb0684f7bc1a7f9074f19ee4d62d797ba81
Peter Jakubco <pjakubco@gmail.com>
2022-07-24 09:58:14 +0100

    Simplify CPU interrupt support

--- src/main/java/net/emustudio/emulib/plugins/cpu/CPUContext.java
+++ src/main/java/net/emustudio/emulib/plugins/cpu/CPUContext.java
@@ -24,7 +24,7 @@ import net.emustudio.emulib.plugins.device.DeviceContext;
 
 /**
  * CPU context can be used by plugins which are connected to CPU.
- *
+ * <p>
  * Custom CPU contexts can extend the runtime functionality accessible to plugins. Plugins which need the specific
  * CPU contexts, should declare a dependency on the CPU plugin.
  */
@@ -33,51 +33,31 @@ import net.emustudio.emulib.plugins.device.DeviceContext;
 public interface CPUContext extends Context {
 
     /**
-     * Determine whether this CPU supports raw interrupts.
-     *
-     * @return true, if raw interrupts are supported, false otherwise
-     */
-    boolean isRawInterruptSupported();
-
-    /**
-     * Send raw interrupt signal to the CPU.
-     *
-     * Does nothing if raw interrupts are not supported.
-     *
-     * @param device Device that interrupts the CPU
-     * @param data interrupt data
-     */
-    void signalRawInterrupt(DeviceContext<?> device, byte []data);
-
-    /**
      * Determine whether this CPU supports interrupts.
      *
      * @return true, if interrupts are supported, false otherwise
      */
-    boolean isInterruptSupported();
+    default boolean isInterruptSupported() {
+        return false;
+    }
 
     /**
      * Send interrupt signal to the CPU.
      *
      * @param device Device that interrupts the CPU
-     * @param mask interrupt mask
+     * @param data   data bus
      */
-    void signalInterrupt(DeviceContext<?> device, int mask);
+    default void signalInterrupt(DeviceContext<?> device, byte[] data) {
 
-    /**
-     * Cancel sending interrupt signal if it was not handled yet.
-     *
-     * @param device Device that wants to cancel the interrupt signal
-     * @param mask clear interrupt mask
-     */
-    void clearInterrupt(DeviceContext<?> device, int mask);
+    }
 
     /**
      * Get CPU frequency in kHz
      *
      * @return CPU frequency in kHz or 0 if it is not supported
      */
-    int getCPUFrequency();
-
+    default int getCPUFrequency() {
+        return 0;
+    }
 }
 
